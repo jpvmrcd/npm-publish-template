3cc038a9b6220eebe6477ddd6c2d22b56299dc55
jpvmrcd <jpvmrcd+git@gmail.com>
2021-06-18 22:12:39 +0800

    improve code coverage

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,3 +4,9 @@ test('null element should throw error', () => {
   const test = new PublishTest(document.createElement('div'), { isTrue: true });
   expect(test).not.toBeNull();
 });
+
+test('test() should return the element', () => {
+  const div = document.createElement('div');
+  const test = new PublishTest(div, { isTrue: true });
+  expect(test.test()).toEqual(div);
+});
